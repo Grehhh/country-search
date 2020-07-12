3d311e62749e5fef3e3759b54c56f87799db2f16
Graham <grh.mrg@gmail.com>
2020-07-12 08:01:15 +0200

    working map

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Country } from 'src/app/interfaces/country';
 import { CountrySearchService } from '../../services/country-search.service'
 import { ActivatedRoute } from '@angular/router'
@@ -11,8 +11,11 @@ import { Location } from '@angular/common';
 })
 export class CountryDetailsComponent implements OnInit {
   @Input() country: Country;
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;     //#mapcontainer en html
+
   languages: string[] = [];
   langToShow: string;
+  latlng: number[] = [];
 
   constructor(private countryService: CountrySearchService, private route:ActivatedRoute, private location: Location) { }
 
@@ -22,15 +25,39 @@ export class CountryDetailsComponent implements OnInit {
     .subscribe(data => {
       this.country = data;
       console.log('country:', this.country);
-      // console.log('name:', this.country[0].name);
       for(let i = 0; i < this.country[0].languages.length; i++) {
-        // console.log('languages:', this.country[0].languages[i].name);
         this.languages.push(this.country[0].languages[i].name);     //en caso de no acceder a la propiedad name, no se puede hacer el join()
       }
-      this.langToShow = this.languages.join(', ');
-      // console.log(this.langToShow);
-      console.log('toGerman',this.country[0].translations.de )
- 
+      this.langToShow = this.languages.join(', '); 
+      // console.log(this.country[0].latlng);
+      // console.log(this.country[0].latlng[0]);
+      // console.log(this.country[0].latlng[1]);
+    });
+  }
+
+  mapInitializer():void {
+    let lat = 0;
+    let lng = 0;
+    const name = this.route.snapshot.paramMap.get('name');
+    this.countryService.getCountry(name)
+    .subscribe(data => {
+      this.country = data;
+      lat = this.country[0].latlng[0];
+      lng = this.country[0].latlng[1];
+      // console.log(this.country[0].latlng);
+      console.log(this.country[0].latlng[0]);
+      console.log(this.country[0].latlng[1]);
+      const coordinates = new google.maps.LatLng(lat, lng);
+      const mapOptions: google.maps.MapOptions = {
+        center: coordinates,
+        zoom: 8
+      };
+      const map = new google.maps.Map(this.gmap.nativeElement, mapOptions);
+      const marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+      marker.setMap(map);
     });
   }
 
@@ -38,8 +65,14 @@ export class CountryDetailsComponent implements OnInit {
     this.location.back();
   }
 
+
+
   ngOnInit() {
     this.getCountry();
+  }
+
+  ngAfterViewInit() {
+    this.mapInitializer();
   }
 
 }
